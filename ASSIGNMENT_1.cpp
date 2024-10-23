9e887c75bc377a4cea8218b2e379f395d4e627ce
AryanKumbhar <aryan.kumbhar23@pccoepune.org>
2024-10-23 10:44:52 +0530

    Update ASSIGNMENT_1.cpp

--- ASSIGNMENT_1.cpp
+++ ASSIGNMENT_1.cpp
@@ -1,3 +1,15 @@
+/* 
+ASSIGNMENT NO :- 1
+
+Assignment Title :-
+Consider a student database of SY COMP class (at least 15 records). Database contains different fields of every student like Roll No, Name and SGPA. 
+a. Design a roll call list, arrange list of students according to roll numbers in ascending order using Insertion Sort
+b. Arrange list of students alphabetically using shell sort
+c. Arrange list of students to find out first ten toppers from a class using Radix sort
+
+*/
+
+
 #include<iostream>
 using namespace std;
 class student
