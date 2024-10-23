f396f925fa2d096e9468be35ad6e364c6633b984
AryanKumbhar <aryan.kumbhar23@pccoepune.org>
2024-10-23 10:45:38 +0530

    Update ASSIGNMENT_2.cpp

--- ASSIGNMENT_2.cpp
+++ ASSIGNMENT_2.cpp
@@ -1,3 +1,14 @@
+/*
+
+ASSIGNMENT :-2
+
+Assignment Title :-
+Consider Employee database of PCCOE (at least 20 records). Database contains different fields of every employee like EMP-ID, EMP-Name and EMP-Salary. 
+a. Arrange list of employees according to EMP-ID in ascending order using Quick Sort
+b. Arrange list of Employee alphabetically using Merge Sort
+
+*/
+
 #include<iostream>
 using namespace std;
 
